6473c8a5d0a8549ab28632409f562188455e9ab0
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-06 11:06:57 -0500

    Another test, cleanup

--- strictEquals.js
+++ strictEquals.js
@@ -1,9 +1,17 @@
 const strictEquals = (numOne, numTwo) => {
   /*    
      Check all the numbers here and return SOMETHING
+
+    SWITCH STATEMENT! 
+
+    Same type, same value
+    Same type, different value
+    Different type, same value
+    Different type, different value
+
   */
   //return false //passes 3/4
-  return true //fails 3/4
+  //return true //fails 3/4
 }
 
 module.exports = strictEquals
--- tests/strictEquals.test.js
+++ tests/strictEquals.test.js
@@ -5,7 +5,7 @@ const strictEquals = require('../strictEquals')
 
 describe('Equals', () => {
   describe('strictEquals', () => {
-    it('returns true if both paramaters ARE NOT equal integers', () => {
+    it('returns true if both paramaters ARE NOT equal OR integers', () => {
       const checkNumberOne = 5
       const checkNumberTwo = '5'
 
@@ -14,7 +14,7 @@ describe('Equals', () => {
       // Test Data shoud return false - both test items are not INT.
       expect(checkResult).to.equal(false)
     })
-    it('returns true if both paramaters ARE NOT equal integers', () => {
+    it('returns true if both paramaters ARE NOT equal OR integers', () => {
       const checkNumberOne = 'a'
       const checkNumberTwo = 7
 
@@ -29,18 +29,15 @@ describe('Equals', () => {
 
       const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
 
-      // Test Data shoud return false - both test items are not INT.
+      // Test Data shoud return false - both test items are not EQUAL.
       expect(checkResult).to.equal(false)
     })
     it('returns true if both paramaters ARE equal integers', () => {
-      //create test data
       const checkNumberOne = 5
       const checkNumberTwo = 5
 
-      // call the function with the test data
       const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
 
-      // make assertions about what should be true
       // Test Data shoud return true - both test items are equal INT.
       expect(checkResult).to.equal(true)
     })
