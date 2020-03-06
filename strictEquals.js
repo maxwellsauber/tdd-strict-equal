6fb20a046d588e836b821faa564dcb936ef9c64e
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-06 10:24:40 -0500

    another test

--- strictEquals.js
+++ strictEquals.js
@@ -2,8 +2,8 @@ const strictEquals = (numOne, numTwo) => {
   /*    
      Check all the numbers here and return SOMETHING
   */
-//return false //passes
- return true //fails
+  return false //passes
+  //return true //fails
 }
 
 module.exports = strictEquals
--- tests/strictEquals.test.js
+++ tests/strictEquals.test.js
@@ -17,6 +17,18 @@ describe('Equals', () => {
       // Test Data shoud return false - both test items are not INT.
       expect(checkResult).to.equal(false)
     })
+    it('returns true if both paramaters ARE NOT equal integers', () => {
+      //create test data
+      const checkNumberOne = 'a'
+      const checkNumberTwo = 7
+
+      // call the function with the test data
+      const checkResult = strictEquals(checkNumberOne, checkNumberTwo)
+
+      // make assertions about what should be true
+      // Test Data shoud return false - both test items are not INT.
+      expect(checkResult).to.equal(false)
+    })
     it('returns true if both paramaters ARE equal integers', () => {
       //create test data
       const checkNumberOne = 5
