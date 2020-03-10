feadc161553e8f5b52ab4ff14b243292b6ac2f3f
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-09 20:48:00 -0400

    clean up test cases

--- tests/strictEquals.test.js
+++ tests/strictEquals.test.js
@@ -5,23 +5,15 @@ const strictEquals = require('../strictEquals')
 
 describe('Equals', () => {
   describe('strictEquals', () => {
-    it('returns true if both paramaters ARE NOT equal OR numbers, BUT are loosely equal', () => {
-      const checkValueOne = '5'
+    it('returns true when given equal values of the same type', () => {
+      const checkValueOne = 5
       const checkValueTwo = 5
 
       const checkResult = strictEquals(checkValueOne, checkValueTwo)
 
-      expect(checkResult).to.equal(false)
+      expect(checkResult).to.equal(true)
     })
-    it('returns true if both paramaters ARE NOT equal OR numers', () => {
-      const checkValueOne = 'a'
-      const checkValueTwo = 7
-
-      const checkResult = strictEquals(checkValueOne, checkValueTwo)
-
-      expect(checkResult).to.equal(false)
-    })
-    it('returns true if both paramaters ARE NOT equal numbers', () => {
+    it('returns fale when given unequal values of the same type', () => {
       const checkValueOne = 42
       const checkValueTwo = 7
 
@@ -29,25 +21,17 @@ describe('Equals', () => {
 
       expect(checkResult).to.equal(false)
     })
-    it('returns true if both paramaters ARE equal numbers', () => {
-      const checkValueOne = 5.5
-      const checkValueTwo = 5.5
+    it('returns false when given equal values of different types', () => {
+      const checkValueOne = '5'
+      const checkValueTwo = 5
 
       const checkResult = strictEquals(checkValueOne, checkValueTwo)
 
-      expect(checkResult).to.equal(true)
+      expect(checkResult).to.equal(false)
     })
-    it('returns true if both paramaters ARE equal strings', () => {
+    it('returns false when given different values of different types', () => {
       const checkValueOne = 'WOOT'
-      const checkValueTwo = 'WOOT'
-
-      const checkResult = strictEquals(checkValueOne, checkValueTwo)
-
-      expect(checkResult).to.equal(true)
-    })
-    it('returns true if both paramaters ARE NOT equal strings', () => {
-      const checkValueOne = 'Lemur'
-      const checkValueTwo = 'Hamster'
+      const checkValueTwo = true
 
       const checkResult = strictEquals(checkValueOne, checkValueTwo)
 
