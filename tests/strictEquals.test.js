82b1b2aa5c51fe03f79ade7920f33ee07b077366
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-03-09 20:54:22 -0400

    another typo

--- tests/strictEquals.test.js
+++ tests/strictEquals.test.js
@@ -21,7 +21,7 @@ describe('Equals', () => {
 
       expect(checkResult).to.equal(false)
     })
-    it('returns false when given equal values of different types', () => {
+    it('returns false when given equal values of different type', () => {
       const checkValueOne = '5'
       const checkValueTwo = 5
 
@@ -29,7 +29,7 @@ describe('Equals', () => {
 
       expect(checkResult).to.equal(false)
     })
-    it('returns false when given unequal values of different types', () => {
+    it('returns false when given unequal values of different type', () => {
       const checkValueOne = 'WOOT'
       const checkValueTwo = true
 
